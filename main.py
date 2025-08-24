8cd3965b300e7355e6565f68b85a0156efbe3628
Shreya <shreyathapliyal29@gmail.com>
2025-08-25 00:47:43 +0530

    Second Edit

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #main.py
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+    return render_template('index.html')
+    if request.method == 'POST':
+        
