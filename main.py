5500e5aad35fa0b2986803c566ed2c0eb5f43371
Shreya <shreyathapliyal29@gmail.com>
2025-08-25 00:50:06 +0530

    Third Edit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+items=[]
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-    return render_template('index.html')
+        return render_template('index.html')
     if request.method == 'POST':
-        
+        items.append('name','Ph.no)
